708a589e0e9df37edf77b640a2fc243e776ab241
error404-sp <shreyasipatil404@gmail.com>
2021-05-06 06:53:55 +0530

    Day 14-16 update

---
+++ Day 16/main.js
@@ -0,0 +1,15 @@
+// JAVASCRIPT ARRAYS
+
+let carNames = ["BMW","Volvo","i10"]
+
+//array is a variable which can hold more than one value
+
+//ACCESSING ARRAYS
+
+let newCarName = carNames[2];
+
+document.getElementById("demo").innerHTML = carNames[0];
+
+//changing a array value
+carNames[1] = "IBM";
+
