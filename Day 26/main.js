69d4eb59699da0642a6d65877f30d29b82985f88
error404-sp <shreyasipatil404@gmail.com>
2021-07-06 15:49:46 +0530

    Rock paper scissor ui

---
+++ Day 26/main.js
@@ -0,0 +1,14 @@
+// Fork a GitHub repository.
+//git clone https://github.com/openvswitch/openvswitch.github.io.git
+// Clone the forked repository to your local system.
+// Add a Git remote for the original repository.
+//git remote add upstream https://github.com/openvswitch/openvswitch.github.io.git
+// Create a feature branch in which to place your changes.
+//git checkout -b <new branch name>
+// Make your changes to the new branch.
+// Commit the changes to the branch.
+//git push origin new-feature
+// Push the branch to GitHub.
+// Open a pull request from the new branch to the original repo.
+// Clean up after your pull request is merged
+
