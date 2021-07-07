dd4426c4ea41ff74a1c2013ccac54ecf9a8717e5
error404-sp <shreyasipatil404@gmail.com>
2021-07-07 21:22:34 +0530

    event bubbling,capture,propagation

--- Day 27/main.js
+++ Day 27/main.js
@@ -1,9 +1,12 @@
 const divs = document.querySelectorAll('div');
 divs.forEach(div =>{
-    capture:true;
+    
     div.addEventListener('click',e=>{
         
-        console.log(div.classList.value); 
-        //this will cause bubbling
-    });
+        console.log(div.classList.value);         //this will cause bubbling
+        e.stopPropagation(); //stops bubbling
+        
+    },{capture:true,
+    once:true}); //this is event capturing i.e activate captures and the second one makes sure it can be clicked only once
+    
 });
