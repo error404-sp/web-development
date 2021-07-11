bd65640a7558e20107171fe6cc7be5f0d67c7d58
error404-sp <shreyasipatil404@gmail.com>
2021-07-11 07:13:41 +0530

    initial commit

---
+++ Day 31/main.js
@@ -0,0 +1,4 @@
+const textArea = document.querySelector("textarea");
+const btnPrimary = document.querySelector(".btn-primary");
+const inputBtn = document.querySelector("#input-btn");
+const translator = document.querySelector("input[name =translator]");
