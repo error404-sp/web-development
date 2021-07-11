2b4ce62104da49b4290cb7cc9168db267ab235b2
error404-sp <shreyasipatil404@gmail.com>
2021-07-11 12:26:50 +0530

    API call and fetching API

--- Day 31/main.js
+++ Day 31/main.js
@@ -1,4 +1,19 @@
-const textArea = document.querySelector("textarea");
-const btnPrimary = document.querySelector(".btn-primary");
-const inputBtn = document.querySelector("#input-btn");
-const translator = document.querySelector("input[name =translator]");
+const textInput = document.querySelector('#textinput');
+const translateBtn = document.querySelector('#translate-btn');
+const output = document.querySelector('.output');
+
+let api = "https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(text){
+   return `${api}?text=${text}`;
+}
+
+translateBtn.addEventListener('click', clickHandler);
+
+function clickHandler(){
+    let inputText = textInput.value;
+    fetch(constructUrl(inputText))
+    .then(response => response.json())
+    .then(json => {output.innerText = json.contents.translated;})
+
+}
